97a25c17aeb835944ebecb87aa26957e2f9e1256
Sam Samiee <samsamiee00@gmail.com>
2025-08-08 00:55:12 +0200

    Add environment variables to GitHub Actions workflow

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -6,13 +6,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
-	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "placeholder",
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "placeholder",
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "placeholder",
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "placeholder",
+	messagingSenderId:
+		process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "placeholder",
+	appId: process.env.REACT_APP_FIREBASE_APP_ID || "placeholder",
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "placeholder",
 };
 
 // Initialize Firebase
